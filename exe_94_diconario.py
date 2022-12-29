33d11697d307103f48ca9cb865d510ce8a972d1b
Jeanbernardo20 <76752986+Jeanbernardo20@users.noreply.github.com>
2022-12-29 15:26:18 -0500

    Update exe_94_diconario.py

--- exe_94_diconario.py
+++ exe_94_diconario.py
@@ -1,24 +1,34 @@
 # criar um dicionario vazio 
 galera = list()
 pessoa = dict()
+soma = media = 0
 
 while True:
-    galera.clear()
-    pessoa['nome'] = str(input('Qual seu Nome: '))
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
     while True:
         pessoa['sexo'] = str(input('Sexo: [M/F]: ')).upper()[0]
         if pessoa['sexo'] in 'MF':
             break
         print('ERRO! Por favor, digite M ou F')
     pessoa['idade'] = int(input('idade: '))
+    soma += pessoa['idade']
     galera.append(pessoa.copy())
     while True:
         resp = str(input('deseja continuar? [S/N]: ')).upper()[0]
         if resp in 'SN':
             break
-        print('ERRO!, responda S ou N')
+        print('ERRO! Responda apenas S ou N')
     if resp == 'N':
         break
 print('-='*30)
 
-print(f' Ao todo temos {len(galera)} cadastrado')  
+print(f' Ao todo temos {len(galera)} pessoas cadastradas') 
+media = soma / len(galera) 
+print(f' A média de idade é de {media:5.2f} anos. ')
+print('C) As mulheres cadastradas foram' end='')
+print()
+for p in galera:
+    if p['sexo'] in 'Ff':
+        print(f'{p["nome"]}', end='')
+print()
