cd2dad366891fcb6c9cbf5d75599ba18fe1459c8
Jeanbernardo20 <76752986+Jeanbernardo20@users.noreply.github.com>
2023-01-02 10:07:26 -0500

    exercicios atualizaos

--- exe_97_def.py
+++ exe_97_def.py
@@ -1,3 +1,16 @@
 '''
 Faça um programa que tenha uma função chamada Escreva(), que recebe um texto qualquer como parametro e mostre uma mensagem com tamanho adaptável
 '''
+# função
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+# programa principal
+escreva('Jean Bernardo')
+escreva('Curso de Python no youtube')
+escreva('Cev')
+
